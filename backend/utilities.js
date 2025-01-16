63419512f16faeb8412e2497846f774f21a97fda
Het Patel <patelhet263@gmail.com>
2025-01-16 15:12:48 +0530

    Initial commit

---
+++ backend/utilities.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    
+    if (!token) {
+        return res.status(401).json({ error: "Token missing or invalid" });
+    }
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: "Invalid or expired token" });
+        }
+        req.user = user;
+        next();
+    });
+    // console.log("Authorization Header:", authHeader);
+}
+
+module.exports = {
+    authenticateToken,
+};
